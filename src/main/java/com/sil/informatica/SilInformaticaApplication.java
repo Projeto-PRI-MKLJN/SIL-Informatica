69dc8ce1a0697c207cef3323a1a57967a6f84d92
MarcosKauanS <kauanplg2020@gmail.com>
2026-03-18 08:27:27 -0300

    chore(init): inicialização oficial do projeto SIL-Informática

---
+++ src/main/java/com/sil/informatica/SilInformaticaApplication.java
@@ -0,0 +1,13 @@
+package com.sil.informatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilInformaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilInformaticaApplication.class, args);
+	}
+
+}
